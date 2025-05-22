de21bd13d7370aa6f3d83552b1b9f5c7170d2b98
maisolo22 <160590080+maisolo22@users.noreply.github.com>
2025-05-22 10:28:11 +0100

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -5,8 +5,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://unhided222.onrender.com"     # non mettere lo / finale al link
+MFPPSW = "123"
 
 # Costanti
 M3U8_OUTPUT_FILE = "247ita.m3u8"
